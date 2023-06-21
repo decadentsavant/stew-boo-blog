4d0f88277e81d3e9fe7ce3344fa3090e759e665d
Corey Stewart <decadentsavant@gmail.com>
2023-06-21 10:30:35 -0400

    Added AstroIconSomethignOrOther package and Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-			}
-		},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {},
+    plugins: [],
+};
