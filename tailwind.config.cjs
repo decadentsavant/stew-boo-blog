9ec53bc75a5c770bec67ae8a598fef6378af6c36
Corey Stewart <decadentsavant@gmail.com>
2023-06-21 18:00:28 -0400

    After 10 rounds with GPT we got the navbar working correctly

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    theme: {},
-    plugins: [],
+    theme: {
+        //  Enable background-clip utilities
+        backgroundClip: ["text"],
+        extend: {
+            colors: {
+                "main-blue": "rgb(89, 203, 230)",
+                "main-purple": "rgb(90, 118, 186)",
+            },
+        },
+    },
+    plugins: [require("@tailwindcss/typography")],
 };
