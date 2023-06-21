d1ec956323c99f564fa7b3f00a84e2c063daa46a
Corey Stewart <decadentsavant@gmail.com>
2023-06-21 10:32:01 -0400

    Deleted some files...modified some files...didn't git diff anything before this commit

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,10 @@
-document.querySelector(".hamburger").addEventListener("click", () => {
-    document.querySelector(".nav-links").classList.toggle("expanded");
+const hamburgerIcon = document.querySelector("#hamburger-icon");
+const navigationMenu = document.querySelector("#navigation-menu");
+
+hamburgerIcon.addEventListener("click", () => {
+    if (navigationMenu.classList.contains("hidden")) {
+        navigationMenu.classList.remove("hidden");
+    } else {
+        navigationMenu.classList.add("hidden");
+    }
 });
